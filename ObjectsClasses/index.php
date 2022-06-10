4ea2c53e847b1d45fde263134a2a85fd70724880
manixcoder <pathakmanish86@gmail.com>
2022-06-10 11:44:53 -0700

    Class and objects

---
+++ ObjectsClasses/index.php
@@ -0,0 +1,20 @@
+<?php
+class TV {  // class with name
+    public $model="xyz";
+    public $valume = 1;
+
+    function valumeUp(){
+        $this->vlume++;
+    }
+    function valumeDown(){
+        $this->valume--;
+    }
+}
+
+$tv_one = new TV; // Object declear 
+$tv_two = new TV; // Object declear 
+
+echo $tv_one->valume;
+
+
+?>
